9d83f67db906528256a90502c6ac6a94e4c76a40
Harbin <harbinramo@gmail.com>
2021-02-03 16:04:12 -0500

    Updates as of 2021-02-03: 1. Updated server/config files. 2. Added server/config files: auth and userConfig. 3. Added packages: connect-flash, cookie-parser, express-session, jsonwebtoken, passport, passport-jwt, and passport-local. 4. Updated server/route files.

--- server/config/app.js
+++ server/config/app.js
@@ -1,16 +1,43 @@
 let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
 let cors = require('cors');
+let auth = require('./auth');
+let database = require('./db');
+let userConfig = require('./userConfig');
 
 const App = () => {
     let app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
     app.use(cors());
 
     let PORT = 4000;
     app.listen(PORT, () => {
         console.log(`Your server is running on port ${PORT}`);
     })
+
+    // ** Set authentication modules
+    app.use(auth.session({
+        secret: database.Secret, 
+        saveUninitialized: false, 
+        resave: false }));
+    app.use(auth.flash());
+    app.use(auth.passport.initialize());
+    app.use(auth.passport.session());
+
+    // ** Set passport user configuration
+    userConfig.SetPassport({
+        passport: auth.passport, 
+        extractJWT: auth.extractJWT, 
+        strategyJWT: auth.strategyJWT,
+        dbSecret: database.Secret
+    });
+
+    // ** Set landing page
+    //app.use(express.static(path.join(__dirname, '../../client/public/index.html')));
+    //app.use(express.static('../client/', {index: 'login.html'}));
 
     // ** Initialize routes
     let usersRouter = require('../routes/user');
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,10 @@ let router = express.Router();
 
 let userController = require('../controllers/user');
 
+router.post('/login', userController.processLogin);
+
 router.post('/register', userController.processRegister);
+
+router.post('/logout', userController.processLogout);
 
 module.exports = router;
